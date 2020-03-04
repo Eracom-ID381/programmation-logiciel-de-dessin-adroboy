e7b3c52a1ba46fb2e191237a28a806bed58381f5
adroboy <asevilla05@hotmail.com>
2020-03-04 15:27:03 +0100

    css html

--- js/sketch.js
+++ js/sketch.js
@@ -7,12 +7,20 @@ function draw() {
     fill(255);
     if (mouseIsPressed) {
         ellipse(width / 2, height / 2, mouseX, mouseY);
+    } else if (mouseX >= 100 && mouseX < 300) {
+        fill(200, 0, 0);
     }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     background(0);
+}
+
+function keyPressed() {
+    if (keyCode === BACKSPACE) {
+        background(0);
+    }
 }
 
 function mouseClicked() {}
