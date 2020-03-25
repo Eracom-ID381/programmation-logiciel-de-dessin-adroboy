d0d656efe3b81e9914ffafaa0253dba3e0262a02
adroboy <asevilla05@hotmail.com>
2020-03-25 17:43:15 +0100

    formes corrects

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,5 @@
 let mode = 0;
-let tri = 30;
+let tri = 90;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -19,31 +19,33 @@ function draw() {
                 stroke(0, 255, 0)
             }
         } else if (mode == 1) {
-            rect(mouseX, mouseY, mouseX / 3, mouseY / 3);
+            rect(mouseX, mouseY, mouseX / 3, mouseX / 3);
             if (mouseX > width / 2) {
-                stroke(250, 250, 0)
+                stroke(250, 0, 0)
             } else {
                 stroke(0, 0, 225)
             }
 
         } else if (mode == 2) {
             let tri = map(mouseX, 0, width, 0, 100);
-            triangle(mouseX, mouseY - tri, mouseX - tri, mouseY + tri, mouseX + tri, mouseY - tri);
-            stroke(125, 0, 125)
-            } else {
-                stroke(0, 100, 255)
-            }
+            triangle(mouseX, mouseY - tri, mouseX - tri, mouseY + tri, mouseX + tri, mouseY + tri);
 
+            if (mouseX > width / 2) {
+                stroke(125, 0, 125)
+            } else {
+                stroke(250, 250, 0)
+            }
         }
     }
-    // if (mouseIsPressed) {
-    //     ellipse(mouseX, mouseY, mouseX / 3, mouseY / 3);
-    //     if (mouseX > width / 2) {
-    //         stroke(255, 100, 0);
-    //     }
-    // } else if (mode === 1) {
-    //     rect(mouseX, mouseY, mouseX / 3, mouseY / 3)
-    // } else if (mode === 2) {}
+}
+// if (mouseIsPressed) {
+//     ellipse(mouseX, mouseY, mouseX / 3, mouseY / 3);
+//     if (mouseX > width / 2) {
+//         stroke(255, 100, 0);
+//     }
+// } else if (mode === 1) {
+//     rect(mouseX, mouseY, mouseX / 3, mouseY / 3)
+// } else if (mode === 2) {}
 
 
 function windowResized() {
